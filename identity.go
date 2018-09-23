55b32dbfdcdcacf6dfe2cdddf1df715b62a53f07
vyzo <vyzo@hackzen.org>
2018-09-23 14:49:00 +0300

    persistent identities for p2pd

---
+++ identity.go
@@ -0,0 +1,25 @@
+package p2pd
+
+import (
+	"io/ioutil"
+
+	crypto "github.com/libp2p/go-libp2p-crypto"
+)
+
+func ReadIdentity(path string) (crypto.PrivKey, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return crypto.UnmarshalPrivateKey(bytes)
+}
+
+func WriteIdentity(k crypto.PrivKey, path string) error {
+	bytes, err := crypto.MarshalPrivateKey(k)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path, bytes, 0500)
+}
--- p2pd/main.go
+++ p2pd/main.go
@@ -6,15 +6,28 @@ import (
 	"fmt"
 	"log"
 
+	libp2p "github.com/libp2p/go-libp2p"
 	p2pd "github.com/libp2p/go-libp2p-daemon"
 )
 
 func main() {
 	sock := flag.String("sock", "/tmp/p2pd.sock", "daemon control socket path")
 	quiet := flag.Bool("q", false, "be quiet")
+	id := flag.String("id", "", "peer identity; private key file")
 	flag.Parse()
 
-	d, err := p2pd.NewDaemon(context.Background(), *sock)
+	var opts []libp2p.Option
+
+	if *id != "" {
+		key, err := p2pd.ReadIdentity(*id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		opts = append(opts, libp2p.Identity(key))
+	}
+
+	d, err := p2pd.NewDaemon(context.Background(), *sock, opts...)
 	if err != nil {
 		log.Fatal(err)
 	}
