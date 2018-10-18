afd21171a0b05d8ed265e7f7a79544ec9d0026fc
Cole Brown <bigswim@gmail.com>
2018-10-18 17:39:43 -0400

    Implement FindPeer

---
+++ p2pclient/dht.go
@@ -0,0 +1,122 @@
+package p2pclient
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+
+	ggio "github.com/gogo/protobuf/io"
+	pb "github.com/libp2p/go-libp2p-daemon/pb"
+	peer "github.com/libp2p/go-libp2p-peer"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+type PeerInfo struct {
+	Id    peer.ID
+	Addrs []ma.Multiaddr
+}
+
+func convertPbPeerInfo(pbi *pb.PeerInfo) (*PeerInfo, error) {
+	id, err := peer.IDFromBytes(pbi.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	addrs := make([]ma.Multiaddr, 0, len(pbi.Addrs))
+	for _, addrbytes := range pbi.Addrs {
+		addr, err := ma.NewMultiaddrBytes(addrbytes)
+		if err != nil {
+			return nil, err
+		}
+		addrs = append(addrs, addr)
+	}
+
+	pi := &PeerInfo{
+		Id:    id,
+		Addrs: addrs,
+	}
+
+	return pi, nil
+}
+
+func newDHTReq(req *pb.DHTRequest) *pb.Request {
+	return &pb.Request{
+		Type: pb.Request_DHT.Enum(),
+		Dht:  req,
+	}
+}
+
+func readDhtResponseStream(ctx context.Context, control net.Conn) <-chan *pb.Response {
+	out := make(chan *pb.Response)
+
+	go func() {
+		defer close(out)
+		defer control.Close()
+
+		r := ggio.NewDelimitedReader(control, MessageSizeMax)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				msg := &pb.Response{}
+				if err := r.ReadMsg(msg); err != nil {
+					log.Errorf("reading FindPeer response: %s", err)
+					return
+				}
+
+				out <- msg
+				if msg.GetType() == pb.Response_ERROR || msg.Dht.GetType() == pb.DHTResponse_END {
+					return
+				}
+			}
+		}
+	}()
+
+	return out
+}
+
+// FindPeer queries the daemon for a peer's address.
+func (c *Client) FindPeer(peer peer.ID) (*PeerInfo, error) {
+	control, err := c.newControlConn()
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(chan PeerInfo, 10)
+	defer close(out)
+	defer control.Close()
+
+	req := newDHTReq(&pb.DHTRequest{
+		Type: pb.DHTRequest_FIND_PEER.Enum(),
+		Peer: []byte(peer),
+	})
+
+	w := ggio.NewDelimitedWriter(control)
+	if err = w.WriteMsg(req); err != nil {
+		control.Close()
+		return nil, err
+	}
+
+	r := ggio.NewDelimitedReader(control, MessageSizeMax)
+	msg := &pb.Response{}
+	if err = r.ReadMsg(msg); err != nil {
+		return nil, err
+	}
+	if msg.GetType() == pb.Response_ERROR {
+		err = fmt.Errorf("error from daemon in findpeer: %s", msg.GetError().GetMsg())
+		return nil, err
+	}
+	dht := msg.GetDht()
+	if dht == nil {
+		return nil, errors.New("dht response was not populated in findpeer")
+	}
+
+	info, err := convertPbPeerInfo(dht.GetPeer())
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
